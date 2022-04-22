9e405b5adddd5bf58721465dfd7c31cc432649bc
GuoJeff <jeff.guo@outlook.com>
2022-04-22 12:57:01 +0200

    updated license for 3.2 releaes

--- unittest_reinvent/scoring_tests/clab_models/__init__.py
+++
@@ -1,4 +0,0 @@
-from unittest_reinvent.scoring_tests.clab_models.test_pip_with_parallel_additive import Test_pip_with_parallel_additive
-from unittest_reinvent.scoring_tests.clab_models.test_pip_with_primary_multiplicative import Test_pip_with_Product
-from unittest_reinvent.scoring_tests.clab_models.test_clab_top_20 import Test_top_20_base, Test_top_20_acid, \
-    Test_top_20_ion_class
